04d913ee57df333607a409497b7d3e88a004aa73
Balaji-V-S <vsbrobot@gmail.com>
2025-10-22 13:58:21 +0530

    feat: adding security configs for ROLE based filtering

--- LumaDesk-backend/src/main/java/com/lumadesk/LumaDeskBackendApplication.java
+++ LumaDesk-backend/src/main/java/com/lumadesk/LumaDeskBackendApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LumaDeskBackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LumaDeskBackendApplication.class, args);
 	}
