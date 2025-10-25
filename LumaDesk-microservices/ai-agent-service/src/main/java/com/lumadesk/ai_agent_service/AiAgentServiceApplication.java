99ce86e888410a6d2913f4d753eabb6f88aed971
Balaji-V-S <vsbrobot@gmail.com>
2025-10-25 21:40:35 +0530

    feat:Configured application services with Eureka Client and local application.yml

--- LumaDesk-microservices/ai-agent-service/src/main/java/com/lumadesk/ai_agent_service/AiAgentServiceApplication.java
+++ LumaDesk-microservices/ai-agent-service/src/main/java/com/lumadesk/ai_agent_service/AiAgentServiceApplication.java
@@ -2,8 +2,10 @@ package com.lumadesk.ai_agent_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AiAgentServiceApplication {
 
 	public static void main(String[] args) {
