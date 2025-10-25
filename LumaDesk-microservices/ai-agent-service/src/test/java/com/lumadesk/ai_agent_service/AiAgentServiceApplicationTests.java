3300873610692e86e040679ee8f2ef1a91e80538
Balaji-V-S <vsbrobot@gmail.com>
2025-10-25 21:07:33 +0530

    feat: Adding TODO:Application Services for LumaDesk

---
+++ LumaDesk-microservices/ai-agent-service/src/test/java/com/lumadesk/ai_agent_service/AiAgentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumadesk.ai_agent_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiAgentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ LumaDesk-microservices/notification-service/src/test/java/com/lumadesk/notification_service/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lumadesk.notification_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
